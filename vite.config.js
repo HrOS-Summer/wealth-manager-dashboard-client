cc916a2e3e155c1d23c1c40266a013fd0ad049c1
Hariom Singh <hariom2083@gmail.com>
2025-08-10 12:38:49 +0530

    fixed proxy server issue

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://wealth-manager-dashboard-server-lk57y95b8-hros-summers-projects.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
