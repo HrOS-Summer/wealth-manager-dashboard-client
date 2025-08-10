4144d5bb853d33ec31ba28bd5ed1f88e8afc718c
Hariom Singh <hariom2083@gmail.com>
2025-08-10 13:10:24 +0530

    update vite configurations

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://wealth-manager-dashboard-server-lk57y95b8-hros-summers-projects.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
